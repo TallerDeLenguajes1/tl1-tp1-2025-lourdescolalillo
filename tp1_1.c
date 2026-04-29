8f799a0b3c6f85662514d81f1ec4e6824a4bb022
lourdes colalillo <lourdescolalillo@gmail.com>
2026-04-29 13:01:25 -0300

    codigo de puntero

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hola mundo");
+    int variable = 10;
+    int *puntero = &variable;
+
+    printf("contenido del puntero: %d\n",*puntero);
+    printf("direccion de memoria almacenada en el puntero: %p\n",(void*)puntero);
+    printf("direccion de memoria de la variable: %p\n",&variable);
+    printf("direccion de memoria del puntero: %p\n",(void*)&puntero);
+    printf("tamanio de la memoria de la variable : %d bytes\n",sizeof(variable));
     return 0;
 }
